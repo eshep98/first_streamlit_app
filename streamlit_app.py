b6b476ea1a8a2553c84b7b31191bf7190b0a8dc0
eshep98 <34072203+eshep98@users.noreply.github.com>
2023-02-16 18:02:58 -0800

    Update streamlit_app.py
    created fruit_choice as a read write input field

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,13 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?',"Kiwi")
+streamlit.write('The user entered', fruit_choice)
+
 
 import requests as rq
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response) #writes data to screen
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
